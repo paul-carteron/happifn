82926c44237c1f30a87b982ad9954b6646a36196
paul-carteron <carteronpaul@gmail.com>
2023-08-23 19:44:41 +0200

    change get_ifn philosophy for simplicity

--- R/get_ifn.R
+++ R/get_ifn.R
@@ -4,10 +4,15 @@
 #' Load IFN dataset(s) into memory. If provided, dataset are loaded from a
 #' directory, else they are seamlessly downloaded before loading.
 #'
-#' @param names `character`; Name of needed dataset.
+#' @usage get_ifn(name,
+#'                dir = NULL,
+#'                quiet = FALSE)
+#'
+#' @param name `character`; Name of needed dataset.
 #' @param dir `character` or `NULL`; connection open for writing.
 #' (ex : "~/test"). If `NULL`, dataset are seamlessly imported into memory.
 #' Be careful; it may take a while.
+#' @param quiet `boolean`; If TRUE no message are displayed.
 #'
 #' @importFrom stats setNames
 #'
@@ -16,37 +21,39 @@
 #'
 #' @examples
 #' \dontrun{
-#' all_dataset <- get_ifn(names = get_dataset_names())
+#' arbre <- get_ifn(name = "arbre", dir = NULL)
+#'
+#' all_dataset <- lapply(get_dataset_names(), get_ifn) |>
+#'                setNames(get_dataset_names())
 #' }
-get_ifn <- function(names = "arbre",
-                    dir = NULL){
+get_ifn <- function(name,
+                    dir = NULL,
+                    quiet = FALSE){
 
   # check input ----
-  # bad dataset
-  bad_names <- !any(names %in% c(get_dataset_names()))
-  if (bad_names){
-    stop("'dataset' should be one of ",
+  # dataset
+  if (length(name) != 1){
+    stop("`name` should be of length 1, not ", length(name), ".", call. = F)
+  }
+
+  bad_name <- !(name %in% get_dataset_names())
+  if (bad_name){
+    stop("`name` should be one of ",
          paste0(get_dataset_names(), collapse = ", "), ".", call. = F)
   }
 
-  # avoid redownload ----
-  global_env_var <- ls(envir = globalenv())
-  already_load <- names[names %in% global_env_var]
-
-  # remove already in memory dataset
-  if (length(already_load) != 0){
-    warning(paste0("`", already_load, "`", collapse = ', '),
-            " not loaded because already in the environment.",
-            call. = F)
-    names <- names[!names %in% already_load]
+  # dir
+  if (!inherits(dir, c("character", "NULL"))){
+    stop("`dir` should be of class `character` or `NULL`", call. = F)
   }
 
-  lapply(names, load_ifn) |>
-    setNames(names) |>
-    list2env(.GlobalEnv) |>
-    invisible()
+  bad_dir <- !is.null(dir) && !dir.exists(dir)
+  if (bad_dir){
+    stop("`dir` does not exist. Check with `dir.exists(dir)`", call. = F)
+  }
 
-  return(NULL)
+  dataset <- load_ifn(name, dir, quiet)
+  return(dataset)
 }
 
 #' load_ifn
@@ -60,15 +67,18 @@ get_ifn <- function(names = "arbre",
 #' @return `data.frame`; dataset
 #' @noRd
 #'
-load_ifn <- function(name,
-                     dir){
+load_ifn <- function(name, dir, quiet){
 
   name <- paste0(toupper(name),".csv")
 
   res <- try(read.csv2(file.path(dir, name)), silent = T)
 
   if(inherits(res, "try-error")){
+    if (!quiet){message("Loading \"", name, "\" dataset...", appendLF = F)}
+
     res <- read.csv2(archive_read(get_url(), name))
+
+    if (!quiet){message(" done.")}
   }
 
   return(res)
--- tests/testthat/test-get_ifn.R
+++ tests/testthat/test-get_ifn.R
@@ -1,29 +1,43 @@
 test_that("get_ifn errors", {
-  expect_error(get_ifn("bad_name"), "'dataset' should")
+
+  expect_error(get_ifn("bad_name"), "`name` should be one")
+  expect_error(get_ifn(NULL), "`name` should be of length 1")
+  expect_error(get_ifn(c("habitat", "arbre")), "`name` should be of length 1")
+
+  expect_error(get_ifn("habitat", dir = 1),
+               "`dir` should be of class `character` or `NULL`")
+  expect_error(get_ifn("habitat", dir = "bad_dir"),
+               "`dir` does not exist. Check with `dir.exists")
 })
 
 test_that("load_ifn works from dir", {
   tmp <- tempdir(check = T)
   write.csv2(data.frame(id = 1), file.path(tmp, "HABITAT.csv"), row.names = F)
+  expect_true("HABITAT.csv" %in% list.files(tmp))
 
-  res <- load_ifn("HABITAT", dir = tmp)
-  expect_equal(res, data.frame(id = 1))
+  habitat <- load_ifn("habitat", dir = tmp, quiet = T)
+  expect_equal(habitat, data.frame(id = 1))
 })
 
 test_that("load_ifn works without dir", {
   skip_if_offline()
 
-  res <- load_ifn("HABITAT", dir = NULL)
+  res <- load_ifn("habitat", dir = NULL, quiet = T)
   expect_equal(class(res), "data.frame")
   expect_equal(dim(res), c(1815L, 14L))
 })
 
-test_that("get_ifn works", {
+test_that("load_ifn quiet parameter", {
   skip_if_offline()
 
-  res <- get_ifn("habitat")
-  expect_null(res)
+  expect_message(load_ifn("habitat", dir = NULL, quiet = F)) |> suppressMessages()
+  expect_no_message(load_ifn("habitat", dir = NULL, quiet = T))
+})
 
-  expect_warning(get_ifn("habitat"),
-                 "not loaded because already in the environment.")
+test_that("get_ifn works without dir", {
+  skip_if_offline()
+
+  res <- get_ifn("habitat", dir = NULL)
+  expect_equal(class(res), "data.frame")
+  expect_equal(dim(res), c(1815L, 14L))
 })
